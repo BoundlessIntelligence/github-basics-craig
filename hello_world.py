d3aae8c518c87ab57b70a5e7943482e5dd6c2972
BoundlessIntelligence <927075@hazelwoodschools.org>
2025-01-27 05:22:34 -0600

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,5 @@
+-# hello_world.py
+# This script prints "Hello, World!" threee times, done by a loop.
 
+for i in range(3): # Causes the number of loops
+  print("Hello, World!") # Prints it within the number of loops
